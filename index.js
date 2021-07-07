e62d1163ba82affb3e78d2cf81bf10c72250a7dc
Parham Javadi <pjfooeve09@yahoo.com>
2021-07-06 20:32:37 -0700

    commit tree 3a3db268189429591b5cec5fd5f7a40582969409

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    var r = /\b\w/gi
+
+    return str.replace(r,(match) => match.toUpperCase())
+  }
+  static sanitize(str) {
+    var r = /[$@!#^*({]/gi
+
+    return str.replace(r,'')
+  }
+  static titleize(str) {
+    let array = str.split(' ')
+    let badArray = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let newArray = []
+    // for (let i = 0; i < array.length; i++) {
+      // if (array.includes(badArray.forEach(word => word))) {
+      //   newArray.push(array[i].charAt(0).toUpperCase() + array[i].slice(1))
+      // } else {
+      //   newArray.push(array[i])
+      // }
+    // }
+    let upcasearray = array.map(word => {
+      if ( word !== 'the' && word !== 'a' && word !== 'an' && word !== 'but' && word !== 'of' && word !== 'and' && word !== 'for' && word !== 'at' && word !== 'by' && word !== 'from') {
+        newArray.push(word.charAt(0).toUpperCase() + word.slice(1))
+      }
+        else {
+          newArray.push(word)
+        }
+    })
+    return newArray.join(' ').charAt(0).toUpperCase() + newArray.join(' ').slice(1)
+  }
 }
